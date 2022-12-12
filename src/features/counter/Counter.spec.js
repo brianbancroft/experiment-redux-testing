2dfbafd9a5dfeba08f258ae38698d83637e16012
brianbancroft <hello@brianbancroft.ca>
2022-12-11 17:56:16 -0800

    Add tests

--- src/features/counter/Counter.spec.js
+++ src/features/counter/Counter.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { act, fireEvent, screen } from "@testing-library/react";
+import { act, fireEvent, screen, waitFor } from "@testing-library/react";
 import { faker } from "@faker-js/faker";
 import { renderWithProviders } from "../../test/renderWithProviders";
 
@@ -72,5 +72,106 @@ describe("Add amount functionality", () => {
     });
 
     expect(counter).toHaveTextContent(amount);
+  });
+});
+
+describe("Add async functionality", () => {
+  test("should add the amount to the counter", async () => {
+    renderWithProviders(<Counter />);
+
+    const amountInput = screen.getByLabelText("Set increment amount");
+    const addAsyncButton = screen.getByText("Add Async");
+    const counter = screen.getByTestId("base-counter");
+
+    expect(counter).toHaveTextContent("0");
+
+    const amount = faker.datatype.number({
+      min: 10,
+      max: 50,
+    });
+
+    // find the Set increment amount input
+    const input = screen.getByLabelText("Set increment amount");
+    fireEvent.change(input, { target: { value: amount } });
+
+    act(() => {
+      amountInput.dispatchEvent(new Event("input"));
+      addAsyncButton.click();
+    });
+
+    // wait for the async action to complete
+    await waitFor(() => {
+      expect(counter).toHaveTextContent(amount);
+    });
+  });
+});
+
+describe("Add if odd functionality", () => {
+  describe("when the counter is odd", () => {
+    test("should add the amount to the counter", () => {
+      renderWithProviders(<Counter />);
+
+      const amountInput = screen.getByLabelText("Set increment amount");
+      const addIfOddButton = screen.getByText("Add If Odd");
+      const counter = screen.getByTestId("base-counter");
+
+      expect(counter).toHaveTextContent("0");
+
+      const amount =
+        2 *
+          faker.datatype.number({
+            min: 10,
+            max: 50,
+          }) +
+        1;
+
+      // find the Set increment amount input
+      const input = screen.getByLabelText("Set increment amount");
+      fireEvent.change(input, { target: { value: amount } });
+
+      const plusButton = screen.getByText("+");
+
+      expect(counter).toHaveTextContent("0");
+
+      act(() => {
+        plusButton.click();
+      });
+
+      act(() => {
+        amountInput.dispatchEvent(new Event("input"));
+        addIfOddButton.click();
+      });
+
+      expect(counter).toHaveTextContent(amount + 1);
+    });
+  });
+  describe("when the counter is even", () => {
+    test("should not add the amount to the counter", () => {
+      renderWithProviders(<Counter />);
+
+      const amountInput = screen.getByLabelText("Set increment amount");
+      const addIfOddButton = screen.getByText("Add If Odd");
+      const counter = screen.getByTestId("base-counter");
+
+      expect(counter).toHaveTextContent("0");
+
+      const amount =
+        2 *
+        faker.datatype.number({
+          min: 10,
+          max: 50,
+        });
+
+      // find the Set increment amount input
+      const input = screen.getByLabelText("Set increment amount");
+      fireEvent.change(input, { target: { value: amount } });
+
+      act(() => {
+        amountInput.dispatchEvent(new Event("input"));
+        addIfOddButton.click();
+      });
+
+      expect(counter).toHaveTextContent("0");
+    });
   });
 });
